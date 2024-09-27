c52d36484b3b75574d479624e9c6f5240d4b1cdd
MuhammadAyan67 <ayan46013@gmail.com>
2024-09-27 20:28:52 +0500

    second project

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import About from "@/component/about";
+import Header from '@/component/header';
+
+
+const page = () => {
+  return (
+    <div>
+     
+        <Header/>
+         <h1>
+        Home page
+      </h1>
+        <About/>
+
+    </div>
+  )
+}
+
+export default page
